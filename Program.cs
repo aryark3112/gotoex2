4c56f188d92f2b658fa83f4e2964f51ab6573b3a
aryark <aryark3112@gmail.com>
2023-07-11 12:35:17 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gotoex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 6)
+                {
+                    goto end;
+                }
+                Console.WriteLine("i value: {0}", i);
+            }
+                       end: Console.WriteLine("The end");
+                    }
+                }
+            }
+        
+    
+
